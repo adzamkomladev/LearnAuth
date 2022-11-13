44f3d787e0f4fcf9e22c9c1dd0971cb81815f0d0
Komla Adzam <adzamkomla.dev@gmail.com>
2022-11-13 17:24:46 +0000

    add protection for auth cookie

--- Part1/Program.cs
+++ Part1/Program.cs
@@ -4,24 +4,32 @@
  * - Authentication Cookie
  */
 
+using Microsoft.AspNetCore.DataProtection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDataProtection();
+
 var app = builder.Build();
 
-app.MapGet("/username", (HttpContext ctx) =>
+app.MapGet("/username", (HttpContext ctx, IDataProtectionProvider idp) =>
 {
+    var protector = idp.CreateProtector("auth-cookie");
+
     var cookie = ctx.Request.Headers.Cookie.FirstOrDefault(x => x.Contains("auth="));
     var authCookie = cookie?[cookie.IndexOf("auth=", StringComparison.Ordinal)..];
 
-    char[] delimiterChars = {'=', ':'};
-    return authCookie?.Split(delimiterChars) switch
-    {
-        [_, _, var username] => username
-    };
+
+    var protectedPayload = authCookie?.Split("=").Last();
+    var payload = protector.Unprotect(protectedPayload);
+
+    return payload?.Split(":").Last();
 });
 
-app.MapGet("/login", (HttpContext ctx) =>
+app.MapGet("/login", (HttpContext ctx, IDataProtectionProvider idp) =>
 {
-    ctx.Response.Headers["set-cookie"] = "auth=usr:komla";
+    var protector = idp.CreateProtector("auth-cookie");
+    ctx.Response.Headers["set-cookie"] = $"auth={protector.Protect("usr:komla")}";
     return "ok";
 });
 
