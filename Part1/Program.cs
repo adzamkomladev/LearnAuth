bbee602f21f53ffaed8ed0b1bca68cf294543177
Komla Adzam <adzamkomla.dev@gmail.com>
2022-11-13 13:22:54 +0000

    add username cookie addition and retireval

--- Part1/Program.cs
+++ Part1/Program.cs
@@ -1,6 +1,28 @@
+/*
+ * In this part I did:
+ *
+ * - Authentication Cookie
+ */
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/username", (HttpContext ctx) =>
+{
+    var cookie = ctx.Request.Headers.Cookie.FirstOrDefault(x => x.Contains("auth="));
+    var authCookie = cookie?[cookie.IndexOf("auth=", StringComparison.Ordinal)..];
+
+    char[] delimiterChars = {'=', ':'};
+    return authCookie?.Split(delimiterChars) switch
+    {
+        [_, _, var username] => username
+    };
+});
+
+app.MapGet("/login", (HttpContext ctx) =>
+{
+    ctx.Response.Headers["set-cookie"] = "auth=usr:komla";
+    return "ok";
+});
 
 app.Run();
